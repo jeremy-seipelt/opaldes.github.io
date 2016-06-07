7db275145e562bb3f33f7215db67deb62f4f5610
opaldes <jeremy.seipelt@web.de>
2016-06-07 15:51:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ document.onscroll = function () {
   if (coords.top <= 0) {
     sticky.className = sticky.className.split(' affix').join('');
     sticky.className = sticky.className + " affix" 
-    sticky.style = "top:38px;";
+    sticky.style = "top:38px; width:" + widthToc + "px;";
     console.log(coords)
   }else{
     sticky.className = sticky.className.split(' affix').join('');
