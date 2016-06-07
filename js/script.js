5072a15928a67e70d5adfefbdad912a25a677cc0
opaldes <jeremy.seipelt@web.de>
2016-06-07 14:50:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.onload = function () {
 document.onscroll = function () {
   sticky= document.getElementById("custom-toc");
   coords = sticky.getBoundingClientRect();
-  if (coords.top < 200) {
+  if (coords.top < 40) {
     sticky.className = sticky.className + " affix" 
   } {
     sticky.className = sticky.className.split(" affix").join('');
