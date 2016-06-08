54629e8029a672358344fa4b67ce0bb25ee40efe
opaldes <jeremy.seipelt@web.de>
2016-06-08 10:23:15 +0200

    Update post.js

--- js/post.js
+++ js/post.js
@@ -5,13 +5,17 @@ window.onload = function () {
   panel.appendChild(toc);
 }
 
+bar = document.getElementById("content");
+if (bar.offsetHeight > window.innerHeight ) {
+  bar.classname = bar.classname + " scroll"
+};
 
 document.onscroll = function () {
   sticky = document.getElementById("custom-toc");
   content = document.getElementById("content");
   coords = content.getBoundingClientRect();
   widthToc = sticky.offsetWidth; 
-  if (coords.top <= 0) {
+  if (coords.top <= 0 && windoe.innerWidth > 970) {
     sticky.className = sticky.className.split(' affix').join('');
     sticky.className = sticky.className + " affix" 
     sticky.style = "top:38px; width:" + widthToc + "px;";
